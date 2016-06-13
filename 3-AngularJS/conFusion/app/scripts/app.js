b0d8d354b33e430943087608877378d90dff01be
feinar <feinar@gmail.com>
2016-06-13 13:08:56 +0300

    Assignment 2: provide a form to enable users to submit their comments

--- 3-AngularJS/conFusion/app/scripts/app.js
+++ 3-AngularJS/conFusion/app/scripts/app.js
@@ -155,6 +155,8 @@ angular.module('confusionApp', [])
         .controller('DishCommentController', ['$scope', function($scope) {
             
             //Step 1: Create a JavaScript object to hold the comment from the form
+            $scope.newComment = {author: "", rating: 5, comment: "", date: ""};
+            console.log($scope.newComment);
             
             $scope.submitComment = function () {
                 
