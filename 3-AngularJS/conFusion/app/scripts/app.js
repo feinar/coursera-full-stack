9950ea44d2a6b97674d28d2a10ebbb88b640308d
feinar <feinar@gmail.com>
2016-06-13 11:01:28 +0300

    Week 2 exercise: Angular Forms and Form Validation - form Validation

--- 3-AngularJS/conFusion/app/scripts/app.js
+++ 3-AngularJS/conFusion/app/scripts/app.js
@@ -66,15 +66,33 @@ angular.module('confusionApp', [])
             $scope.toggleDetails = function() {
                 $scope.showDetails = !$scope.showDetails;
             };
+          }])
 
-            .controller('ContactController', ['$scope', function($scope) {
+          .controller('ContactController', ['$scope', function($scope) {
+              $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+              var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+              $scope.channels = channels;
+              $scope.invalidChannelSelection = false;
+          }])
 
-            $scope.feedback = {mychannel:"", firstName:"", lastName:"",
-                               agree:false, email:"" };
-            }])
+          .controller('FeedbackController', ['$scope', function($scope) {
+              $scope.sendFeedback = function() {
+                console.log($scope.feedback);
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                    $scope.feedback.mychannel="";
 
-            .controller('FeedbackController', ['$scope', function($scope) {
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+              };
+          }])
 
-            }]);
+;
             
-        }]);
