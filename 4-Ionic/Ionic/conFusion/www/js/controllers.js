1098a5edff937f8baa9f4d377bcf445561e9b603
feinar <feinar@gmail.com>
2016-07-28 13:33:43 +0300

    4. Week 2 exercise: added option buttons supported by Ionic lists to enable users to add menu items to their favorites

--- 4-Ionic/Ionic/conFusion/www/js/controllers.js
+++ 4-Ionic/Ionic/conFusion/www/js/controllers.js
@@ -74,8 +74,8 @@ angular.module('conFusion.controllers', [])
 
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL',  function($scope, menuFactory, baseURL) {
-    
+.controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
+
     $scope.baseURL = baseURL;
     $scope.tab = 1;
     $scope.filtText = '';
@@ -114,6 +114,13 @@ angular.module('conFusion.controllers', [])
     $scope.toggleDetails = function() {
         $scope.showDetails = !$scope.showDetails;
     };
+
+    $scope.addFavorite = function (index) {
+        console.log("index is " + index);
+        favoriteFactory.addToFavorites(index);
+        $ionicListDelegate.closeOptionButtons();
+    }
+    
 }])
 
 .controller('ContactController', ['$scope', function($scope) {
